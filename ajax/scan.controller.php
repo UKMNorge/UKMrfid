8822b561fca6cfed6a5bfd55cc6666d4cdcf6dad
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 11:15:41 +0000

    Scan direkte, ikke bry deg med person.

--- ajax/scan.controller.php
+++ ajax/scan.controller.php
@@ -17,13 +17,7 @@ $rfid = $_POST['rfidValue'];
 
 // Lag hash av rfid og IP for å sjekke om dette er en OK request
 $hash = sha1($rfid + $guid);
-if ( false && $hash != $_POST['hash'] ) {
-	$JSON->success = false;
-	$JSON->message = "RFID og GUID-hash matcher ikke!";
-	$JSON->data['lokalHash'] = $hash;
-} 
-else if( PersonColl::hasRFID($rfid) ) {
-	$person = PersonColl::getByRFID($rfid);
+if ( true || $hash == $_POST['hash'] ) {
 	try {
 		$scan = Scan::create($rfid, $scanner->getDirection(), $scanner->getArea());
 		$JSON->success = true;
@@ -33,9 +27,9 @@ else if( PersonColl::hasRFID($rfid) ) {
 		$JSON->message = $e->getMessage();
 		$JSON->code = $e->getCode();
 	}
-	
-
 } else {
 	$JSON->success = false;
-	$JSON->message = "Fant ikke personen du prøvde å scanne!";
+	$JSON->message = "RFID og GUID-hash matcher ikke!";
+	$JSON->data['lokalHash'] = $hash;
 }
+
