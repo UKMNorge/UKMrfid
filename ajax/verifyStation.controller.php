19ad76096bc88c54a6552f2ce9a368629673a5b5
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 09:58:48 +0000

    Verify scanner, not station

--- ajax/verifyStation.controller.php
+++ ajax/verifyStation.controller.php
@@ -1,13 +1,23 @@
 <?php
 
-// Station er definert i api.php
-global $station;
-
-if ( $station->isVerified( $_POST['guid'] ) ) {
-	$JSON->success = true;
-	$JSON->message = "Stasjon godkjent!";
-} else {
-	$JSON->success = false;
-	$JSON->message = "Stasjon ikke godkjent enda";
+// Har vi scanner?
+global $scanners;
+global $guid;
+try {
+	$scanner = $scanners->getByGUID($guid);	
+	if ( $scanner->isVerified() ) {
+		$JSON->success = true;
+		$JSON->message = "Stasjon godkjent!";
+	}
+	else {
+		$JSON->success = false;
+		$JSON->message = "Stasjon ikke godkjent enda";
+	}
 }
+catch (Exception $e) {
+	$JSON->success = false;
+	$JSON->message = "Stasjon ikke funnet!";
+}
+
+
 ?>
