8d1f92cd4030363030e51051759248f19f754290
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 10:16:00 +0000

    Scan!

--- ajax/scan.controller.php
+++ ajax/scan.controller.php
@@ -1,11 +1,36 @@
 <?php
-if (false) {
-	$JSON->success = true;
-	$JSON->message = "Scan OK";
-	// TOOD: Trigger GPIO på RPI
-} else {
-	// TOOD: Trigger GPIO på RPI
+
+require_once('UKM/RFID/person.collection.php');
+
+global $scanner;
+
+// Debug
+$JSON->data = $_POST;
+
+$rfid = $_POST['rfidValue'];
+
+// Lag hash av rfid og IP for å sjekke om dette er en OK request
+$hash = sha1($rfid + $_SERVER['REMOTE_ADDR']);
+if ( $hash != $_POST['hash'] ) {
 	$JSON->success = false;
-	$JSON->message = "Bruker har ikke lov inn!";
-	$JSON->data = $_POST;
+	$JSON->message = "RFID og IP-hash matcher ikke!";
+	break;
+}
+
+if( PersonColl::hasRFID($rfid) ) {
+	$person = PersonColl::getByRFID($rfid);
+	try {
+		$scan = Scan::create($rfid, $scanner->getDirection(), $scanner->getArea());
+		$JSON->success = true;
+	} 
+	catch (Exception $e) {
+		$JSON->success = false;
+		$JSON->message = $e->getMessage();
+		$JSON->code = $e->getCode();
+	}
+	
+
+} else {
+	$JSON->success = false;
+	$JSON->message = "Fant ikke personen du prøvde å scanne!";
 }
