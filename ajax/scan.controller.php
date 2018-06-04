612a30192fffd85a1b8ca7b68698715ce519ff3f
Marius Mandal <mariusmandal@gmail.com>
2018-06-04 18:27:10 +0200

    Mer data på ajax respons

--- ajax/scan.controller.php
+++ ajax/scan.controller.php
@@ -4,6 +4,9 @@ require_once('UKM/RFID/person.collection.php');
 require_once('UKM/RFID/scan.class.php');
 
 use UKMNorge\RFID\Scan;
+use UKMNorge\RFID\PersonColl;
+use UKMNorge\RFID\Person;
+use UKMNorge\RFID\Herd;
 
 global $scanner;
 global $guid;
@@ -18,6 +21,11 @@ $hash = sha1($rfid + $guid);
 if ( true || $hash == $_POST['hash'] ) {
 	try {
 		$scan = Scan::create($rfid, $scanner->getDirection(), $scanner->getAreaId());
+		$person = PersonColl::getByRFID( $rfid );
+		$JSON->person = $person->getFirstName();
+		$JSON->herd = $person->getHerd()->getName();
+		$JSON->herd_foreign_id = $person->getHerd()->getForeignId();
+		$JSON->direction = $scanner->getDirection();
 		$JSON->success = true;
 	} 
 	catch (Exception $e) {
